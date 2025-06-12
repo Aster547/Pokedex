386b5412b74859f9e122ba55bd4199b5fa25238f
Aster547 <samuelchristophervisarra@gmail.com>
2025-06-13 02:51:49 +0800

    huh

---
+++ pokedex/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="w-[100%] h-24 bg-red-500 flex items-center">
+        <h1 className="text-4xl font-bold text-[#fbd743]">
+          Pokedex
+        </h1>
+      </div>
+    </div>
+  );
+}
